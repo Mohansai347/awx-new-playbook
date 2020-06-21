f83f6a95a03570ccc392137f1c726184775d4725
Mohansai347 <41725622+Mohansai347@users.noreply.github.com>
2020-06-21 21:00:46 +0530

    Update sample.sh

--- awxplaybook/tasks/sample.sh
+++ awxplaybook/tasks/sample.sh
@@ -1,5 +1,5 @@
-dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-dnf install docker-ce-3:18.09.1-3.el7
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo -y 
+dnf install docker-ce-3:18.09.1-3.el7 -y
 systemctl start docker
 systemctl enable --now docker.service
 alternatives --set python /usr/bin/python3
